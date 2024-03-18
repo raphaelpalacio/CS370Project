634e7dd879ba4b9542be75e7cc7ff8f57c6612db
jlee6422 <jlee6422@gmail.com>
2024-03-18 18:54:14 -0400

    log_in page update

--- login_page/lib/main.dart
+++ login_page/lib/main.dart
@@ -15,10 +15,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pomodoro Plus',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 50, fontFamily: 'Lucida'), 
+          ),
       ),
-      home: const MyHomePage(title: 'Create an Account'),
+      home: const MyHomePage(title: 'Log In'),
     );
   }
 }
@@ -38,32 +41,35 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.blue[900],
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+
+      body: Container( // Wrap Scaffold with Container
+      color: Colors.blue[900],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
-
-            Container(
+            const SizedBox(
               width : 250,
-              child : const TextField(
+              child : TextField(
                 decoration: InputDecoration(
                 labelText: 'Email',
+                labelStyle: TextStyle(color: Colors.white, fontFamily: 'Lucida'),
                 ),
               ),
             ),
             const SizedBox(height: 16),
 
 
-            Container(
+            const SizedBox(
               width : 250,
-              child : const TextField(
+              child : TextField(
                 obscureText: true,
                 decoration: InputDecoration(
                   labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.white, fontFamily: 'Lucida'),
                 ),
               ),
             ),  
@@ -77,6 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    )
     );
   }
 }
