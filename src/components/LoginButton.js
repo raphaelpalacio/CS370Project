ab5c016312ed7a9475196afac46be152b20cf282
Alejandro Pacheco <alepacheco0827@gmail.com>
2024-03-27 17:44:27 -0400

    fixed minor file errors

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,4 +1,3 @@
-// LoginButton.js
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./Navbar.css";
--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,6 +1,3 @@
-
-
-
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
