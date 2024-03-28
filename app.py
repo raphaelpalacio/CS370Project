265fdda08f945ded0ea9fe679e28e957fd8ff544
Hamza Alkadir <haalkad@emory.edu>
2024-03-28 15:57:56 -0400

    added users login

--- app.py
+++ app.py
@@ -2,11 +2,69 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import uuid
+from jose import jwt
+import json 
+from urllib.request import urlopen
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pomodoro.db'  # SQLite database URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+AUTH0_DOMAIN = 'dev-otrfj0d3n15cdjdz.us.auth0.com' # 
+API_AUDIENCE = 'https://dev-otrfj0d3n15cdjdz.us.auth0.com/api/v2/' #
+ALGORITHMS = ['RS256']
+
+# 
+def get_token_auth_header():
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise Exception("Authorization header is missing")
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise Exception("Authorization header must start with Bearer")
+    elif len(parts) == 1:
+        raise Exception("Token not found")
+    elif len(parts) > 2:
+        raise Exception("Authorization header must be Bearer token")
+
+    token = parts[1]
+    return token
+
+def verify_decode_jwt(token):
+    jsonurl = urlopen(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer=f"https://{AUTH0_DOMAIN}/"
+            )
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise Exception("Token expired.")
+        except jwt.JWTClaimsError:
+            raise Exception("Incorrect claims. Please, check the audience and issuer.")
+        except Exception:
+            raise Exception("Unable to parse authentication token.")
+
+    raise Exception("Unable to find appropriate key.")
 
 # Models
 class User(db.Model):
@@ -75,30 +133,51 @@ def register_user():
 
 @app.route('/users/login', mehtods=['POST'])
 def login_user():
-    #data = 
-    user_id = 123  # Assuming the user ID is obtained from the authentication process
-    
-    # Create a JWT token
-    token = jwt.encode({'user_id': user_id}, app.config['SECRET_KEY'], algorithm='HS256')
-
-    return jsonify({'token': token}), 200
+    token = get_token_auth_header()
+    try:
+        payload = verify_decode_jwt(token)
+        return jsonify({"success": True, "message": "User authenticated", "user": payload["sub"]}), 200
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 401
 
 @app.route('/users/profile', methods=['GET'])
 def user_profile():
+    # Authenticate with token
+    token = get_token_auth_header()
+    if not token:
+        return jsonify({"error": "Authorization token is missing"}), 401
 
+    payload = verify_decode_jwt(token)
+    if not payload:
+        return jsonify({"error": "Invalid token"}), 401
 
+    # Search for profile using info in the token
+    user_id = payload.get("sub")  # Assuming the user's unique identifier is in the 'sub' claim
+    user = User.query.filter_by(uID=user_id).first()  # Query the User table for the user ID
+    if not user:
+        return jsonify({"error": "User not found"}), 404
 
-    return
+    user_profile = users_db[user]
+
+    # return username, email, and role
+    return jsonify({
+        "username": user_profile["username"],
+        "email": user_profile["email"],
+        "role": user_profile["role"]
+    }), 200
 
 @app.route('/users/profile/update', methods=['PATCH'])
 def update_profile():
 
+    
     return
 
 @app.route('/users/delete', methods=['DELETE'])
 def delete_user():
 
     return
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensures tables are created before the first request if they don't exist
