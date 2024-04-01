cd3c344ad3bd4f3e284d6d972c962535f8f1577d
anderoochang <andrew.chang.sd@gmail.com>
2024-04-01 17:15:05 -0400

    Homepage Initial Commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,9 +4,12 @@ import "./HomePage.css";
 const HomePage = () => {
   return (
     <div>
-      <div className="home-container">
-        <h1>Welcome to the Homepage</h1>
-        <p>This is the content of the homepage.</p>
+      <div className="home-container-left">
+        <h1>Ditch the Guilt, Not the Group Chat</h1>
+        <p>A study and work application that supports all of your Pomodoro  needs. Get work done in focused bursts and reward yourself with micro-hangouts with friends.</p>
+      </div>
+      <div className="home-container-right">
+        <img src="featureImage.png" alt="Feature Image" />
       </div>
     </div>
   );
