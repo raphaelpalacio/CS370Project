da061a0706fc26e31aef2cc1f1cf4d3e9153ff8b
jlee6422 <jlee6422@gmail.com>
2024-04-30 23:51:11 -0400

    final fix

--- src/pages/Alarm/PauseButton.js
+++ src/pages/Alarm/PauseButton.js
@@ -1,7 +1,7 @@
 function PauseButton(props) {
   return (
     <button {...props}>
-        <div style={{ display: 'flex', justifyContent: 'flex-start', marginLeft: '20px' }}> {/* Adjust marginLeft as needed */}
+        <div style={{ display: 'flex', justifyContent: 'flex-start', marginLeft: '20px' }}> {}
         <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-8" viewBox="2 5 20 15" fill="currentColor">
       <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
       </svg>
